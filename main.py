eb24f1a14850bf3640f78bdd4db4aba7c804b366
b9llach <billy.bellach@gmail.com>
2024-10-11 18:46:03 -0400

    cleaned up files

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-import os
 from translator import Translator
-
-os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
-os.environ["QT_SCALE_FACTOR"] = "1"
-os.environ["QT_SCREEN_SCALE_FACTORS"] = "1"
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
