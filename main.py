6b1688480a118851d7c180ad4d305a08f0e3a564
b9llach <billy.bellach@gmail.com>
2024-10-12 02:38:21 -0400

    updated icon

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from translator import Translator
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app_icon = QIcon("./imgs/icon.png")
+    app.setWindowIcon(app_icon)
     translator = Translator()
     sys.exit(app.exec())
